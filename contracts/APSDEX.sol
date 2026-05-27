9799894303170a690f5b328182556116a4a35df1
peter-mwau <kyalepeter2000@gmail.com>
2026-05-27 08:49:47 +0300

    Updated the APSDEX contract

--- contracts/APSDEX.sol
+++ contracts/APSDEX.sol
@@ -8,12 +8,14 @@ import { APS } from "./APS.sol";
 
 contract APSDEX {
     IERC20 token;
-    uint256 public pricePerShare = 1 ether; // Initial price per share
+
+    uint256 public ethReserve;
+    uint256 public apsReserve;
     uint256 public totalLiquidity;
 
     mapping(address => uint256) public liquidity;
 
-    event LiquidityInitialized(address indexed provider, uint256 amount);
+    event LiquidityInitialized(address indexed provider, uint256 ETH_amount, uint256 APS_amount);
     event LiquidityProvided(address indexed provider, uint256 liquidityMinted, uint256 ethAmount, uint256 tokenAmount);
     event LiquidityRemoved(address indexed provider, uint256 liquidityBurned, uint256 ethAmount, uint256 tokenAmount);
 
@@ -22,16 +24,23 @@ contract APSDEX {
     }
 
     //function to initialize the liquidity pool
-    function initializePool(uint256 _initialLiquidity) external returns (bool) {
-        require(totalLiquidity == 0, "Pool already initialized");
-        require(token.transferFrom(msg.sender, address(this), _initialLiquidity), "Transfer failed");
-        totalLiquidity = _initialLiquidity;
-        liquidity[msg.sender] = _initialLiquidity;
+    function initializePool(uint256 apsAmount) external payable returns (bool) {
+    require(ethReserve == 0 && apsReserve == 0, "Already initialized");
+    require(msg.value > 0 && apsAmount > 0, "Invalid amounts");
 
-        emit LiquidityInitialized(msg.sender, _initialLiquidity);
+    require(token.transferFrom(msg.sender, address(this), apsAmount), "Transfer failed");
 
-        return true;
-    }
+    ethReserve = msg.value;
+    apsReserve = apsAmount;
+
+    totalLiquidity = msg.value; // simple LP init
+
+    liquidity[msg.sender] = msg.value;
+
+    emit LiquidityInitialized(msg.sender, msg.value, apsAmount);
+
+    return true;
+}
 
     //function to get the amount you should receive (xOutput) given the reserves of both tokens in the pool
     function price(uint256 _xInput, uint256 _xReserves, uint256 _yReserves) public pure returns (uint256 yOutput) {
@@ -43,12 +52,8 @@ contract APSDEX {
 
      //function to get the current price of the shares in the pool
     function currentPrice() public view returns (uint256) {
-        uint256 ethReserve = address(this).balance;
-        uint256 tokenReserve = token.balanceOf(address(this));
 
-        require(tokenReserve > 0, "No liquidity");
-
-        return (ethReserve * 1e18) / tokenReserve;
+        return (ethReserve * 1e18) / apsReserve;
     }
 
      //function to calculate the amount of xInput required to get a certain amount of yOutput given the reserves of both tokens in the pool
@@ -62,44 +67,44 @@ contract APSDEX {
      //function to send eth to DEX in exchange for APS tokens
      function ethToToken() internal returns (uint256 tokenOutput) {
         uint256 ethInput = msg.value;
-        require(ethInput > 0, "Must send ETH to swap for tokens");
-        uint256 xReserves = address(this).balance - ethInput; // Exclude the current transaction's ETH
-        uint256 yReserves = totalLiquidity;
-        tokenOutput = price(ethInput, xReserves, yReserves);
+        require(ethInput > 0, "Must send ETH");
+
+        tokenOutput = (ethInput * apsReserve) / ethReserve;
+
         require(token.transfer(msg.sender, tokenOutput), "Token transfer failed");
+
+        ethReserve += ethInput;
+        apsReserve -= tokenOutput;
+
         return tokenOutput;
-     }
+    }
 
     //function to send APS tokens to DEX in exchange for eth
-    function tokenToEth(uint256 _tokenInput) internal returns (uint256 ethOutput) {
-        require(_tokenInput > 0, "Must send tokens to swap for ETH");
-        require(token.balanceOf(msg.sender) >= _tokenInput, "Insufficient token balance");
-        require(token.allowance(msg.sender, address(this)) >= _tokenInput, "Token allowance too low");
-        uint256 xReserves = address(this).balance;
-        uint256 yReserves = totalLiquidity;
-        ethOutput = price(_tokenInput, yReserves, xReserves);
-        require(token.transferFrom(msg.sender, address(this), _tokenInput), "Token transfer failed");
-        (bool success, ) = msg.sender.call{value: ethOutput}("");
-        require(success, "ETH transfer failed");
+    function tokenToEth(uint256 tokenInput) internal returns (uint256 ethOutput) {
+        require(tokenInput > 0, "Must send tokens");
+
+        require(token.transferFrom(msg.sender, address(this), tokenInput), "Transfer failed");
+
+        ethOutput = (tokenInput * ethReserve) / apsReserve;
+
+        payable(msg.sender).transfer(ethOutput);
+
+        apsReserve += tokenInput;
+        ethReserve -= ethOutput;
 
         return ethOutput;
-
     }
 
     //function to allow users to swap between eth and APS tokens
-    function swap(uint256 _tokenInput) external payable returns (uint256 outputAmount) {
-        if (msg.value > 0 && _tokenInput == msg.value) {
-            // User is sending ETH to swap for tokens
-            outputAmount = ethToToken();
-        } else if (_tokenInput > 0) {
-            // User is sending tokens to swap for ETH
-            outputAmount = tokenToEth(_tokenInput);
+    function swap(uint256 tokenInput) external payable returns (uint256 output) {
+        if (msg.value > 0) {
+            output = ethToToken();
         } else {
-            revert("Must send either ETH or tokens to swap");
+            output = tokenToEth(tokenInput);
         }
     }
 
-    // allows deposits of $CORN and $ETH to liquidity pool
+    // allows deposits of $APS and $ETH to liquidity pool
     function deposit() public payable returns (uint256 tokensDeposited) {
         require(msg.value > 0, "Must send value when depositing");
         uint256 ethReserve = address(this).balance - msg.value;
@@ -120,7 +125,7 @@ contract APSDEX {
         return tokenDeposit;
     }
 
-    // allows withdrawal of $CORN and $ETH from liquidity pool
+    // allows withdrawal of $APS and $ETH from liquidity pool
     function withdraw(uint256 amount) public returns (uint256 ethAmount, uint256 tokenAmount) {
         require(liquidity[msg.sender] >= amount, "withdraw: sender does not have enough liquidity to withdraw.");
         uint256 ethReserve = address(this).balance;
